a2a01f9c0c1df5ceca08c77c903f1726cfab36e2
Omo-Tines <77069551+Omo-Tines@users.noreply.github.com>
2025-07-07 22:26:49 +0000

    Fresh commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './pages/About'
 import Footer from './components/Footer'
 import Contact from './pages/Contact'
 import Fevai from './components/Fevai'
-
+import BotpressWebchat from './components/BotpressWebchat'
 
 const App = () => {
   return (
@@ -19,6 +19,7 @@ const App = () => {
         <Route path='/contact' element={<Contact />} />
         <Route path='/fevai' element={<Fevai />} />
       </Routes>
+      <BotpressWebchat />
       <Footer />
     </div>
   )
---
+++ src/components/BotpressWebchat.jsx
@@ -0,0 +1,58 @@
+
+const BUTTON_COLOUR = "#F59E0B"
+const BOT_NAME = "Fevai"
+
+const BotpressWebchat = () => {
+
+  const iframeSrcDoc = `
+    <!doctype html>
+    <html lang="en">
+      <head></head>
+      <body>
+        <script src="https://cdn.botpress.cloud/webchat/v3.0/inject.js"></script>
+        <script defer>
+          // Paste your Webchat config snippet here
+          window.botpress.init({
+            "botId": "9ed1a1da-9c2c-4eda-bd2e-54be08296b82",
+            "configuration": {
+              "version": "v1",
+              "website": {},
+              "email": {},
+              "phone": {},
+              "termsOfService": {},
+              "privacyPolicy": {},
+              "color": "${BUTTON_COLOUR}",
+              "variant": "solid",
+              "headerVariant": "glass",
+              "themeMode": "light",
+              "fontFamily": "inter",
+              "radius": 4,
+              "feedbackEnabled": true,
+              "botName": "${BOT_NAME}",
+              "footer": "[âš¡ by Botpress](https://botpress.com/?from=webchat)",
+              "allowFileUpload": true
+            },
+            "clientId": "${import.meta.env.VITE_WEBCHAT_CLIENT_ID}"
+          });
+        </script>
+      </body>
+    </html>
+  `;
+
+  return (
+    <div className="
+        fixed bottom-5 right-5
+        h-[500px] w-[400px]
+        z-[1000]
+        shadow-lg rounded-lg overflow-hidden
+      " >
+      <iframe
+        className="h-full w-full border-none"
+        srcDoc={iframeSrcDoc}
+        title="Botpress Webchat"
+      ></iframe>
+    </div>
+  );
+};
+
+export default BotpressWebchat;
