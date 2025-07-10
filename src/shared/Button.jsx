6f458012db7b59a8955d7f3787b6d220f6e6f7f4
Omo-Tines <77069551+Omo-Tines@users.noreply.github.com>
2025-07-10 01:29:20 +0100

    Update Button.jsx

--- src/shared/Button.jsx
+++ src/shared/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { FiExternalLink } from 'react-icons/fi'
 
-const Button = ({ title }) => {
+const Button = ({ title, onClick }) => {
   return (
     <button
+  onClick={onClick}
   className="bg-amber-300/80 text-black py-3 px-10 rounded-sm shadow-md hover:bg-amber-500/80 
   transition-colors duration-300 cursor-pointer
   focus:outline-none focus:ring-2 focus:ring-secondary/50 
