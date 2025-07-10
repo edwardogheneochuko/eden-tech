461fbadee915131cf03084552e2d431de536b8f9
Omo-Tines <77069551+Omo-Tines@users.noreply.github.com>
2025-07-10 02:15:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Footer from './components/Footer';
-import Contact from './pages/Contact';
-import Fevai from './components/Fevai';
-import BotpressWebchat from './components/BotpressWebchat';
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Footer from './components/Footer'
+import Contact from './pages/Contact'
+import Fevai from './components/Fevai'
+import BotpressWebchat from './components/BotpressWebchat'
+
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/fevai' element={<Fevai />} />
-        </Routes>
-        <BotpressWebchat />
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/fevai' element={<Fevai />} />
+      </Routes>
+      <BotpressWebchat />
+      <Footer />
+    </div>
+  )
+}
 
-export default App;
+export default App
